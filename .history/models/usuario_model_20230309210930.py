47a54c7e8c49f71c90a7a50db84cb4f0c76d6190
LuisCruzVeldever <luiscruzv@outlook.com>
2023-03-09 21:42:24 -0500

    inicio de proyecto semana 03

---
+++ .history/models/usuario_model_20230309210930.py
@@ -0,0 +1,10 @@
+from bd import conexion
+from sqlalchemy import Column, types
+
+class Usuario(conexion.Model):
+    id = Column(type_=types.Integer, autoincrement=True, primary_key=True)
+    correo = Column(type_=types.Text, unique=True, nulable=False)
+    password = Column(type_=types.Text, nullable=False)
+    nombre = Column(type_=types.Text, nullable=False)
+    apellido = Column(type_=types.Text, nullable=False)
+    __tablename__= 'usuarios'
